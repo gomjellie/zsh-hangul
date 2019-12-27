72f10813da92b373ce810e9ebf571b38da906eae
jacky <gomjellie@gmail.com>
2019-12-27 23:47:07 +0900

    hangul-map works but it has length problem

--- zsh-hangul.zsh
+++ zsh-hangul.zsh
@@ -2,36 +2,6 @@ echo "zsh-hangul loaded"
 
 autoload -Uz _convert_gksdud
 
-typeset -A gksdud # key map for korean-english 2-set array(a.k.a. 두벌식 자판)
-gksdud[ㅂ]="q"
-gksdud[ㅈ]="w"
-gksdud[ㄷ]="e"
-gksdud[ㄱ]="r"
-gksdud[ㅅ]="t"
-gksdud[ㅛ]="y"
-gksdud[ㅕ]="u"
-gksdud[ㅑ]="i"
-gksdud[ㅐ]="o"
-gksdud[ㅔ]="p"
-
-gksdud[ㅁ]="a"
-gksdud[ㄴ]="s"
-gksdud[ㅇ]="d"
-gksdud[ㄹ]="f"
-gksdud[ㅎ]="g"
-gksdud[ㅗ]="h"
-gksdud[ㅓ]="j"
-gksdud[ㅏ]="k"
-gksdud[ㅣ]="l"
-
-gksdud[ㅋ]="z"
-gksdud[ㅌ]="x"
-gksdud[ㅊ]="c"
-gksdud[ㅍ]="v"
-gksdud[ㅠ]="b"
-gksdud[ㅜ]="n"
-gksdud[ㅡ]="m"
-
 _convert_gksdud() {
     # widget to convert korean -> english
     BUFFER="${LBUFFER}${gksdud[${KEYS}]}${RBUFFER}"
