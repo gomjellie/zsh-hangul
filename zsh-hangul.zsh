78c257d5bbaf70dddc342881d3899db07b45fe46
Jacky <gomjellie@gmail.com>
2019-12-29 18:59:46 +0900

    Stop inside quote (#12)
    * stop converting in quote
    
    * fix bug when CURSOR is not in quote

--- zsh-hangul.zsh
+++ zsh-hangul.zsh
@@ -5,14 +5,24 @@ ZSH_HANGUL_DIR=$(dirname $0)
 
 _convert_gksdud() {
     # widget to convert korean -> english
-    BUFFER="${LBUFFER}${gksdud[${KEYS}]}${RBUFFER}"
-    CURSOR+=${#gksdud[${KEYS}]}
+    let n_double_quote=$(echo $LBUFFER |grep -o "\"" |wc -l |xargs)
+    let n_single_quote=$(echo $LBUFFER |grep -o "\'" |wc -l |xargs)
+    let n_back_quote=$(echo $LBUFFER |grep -o "\`" |wc -l |xargs)
+    
+    [[ $(($n_double_quote % 2)) -eq 0 ]] &&
+    [[ $(($n_single_quote % 2)) -eq 0 ]] &&
+    [[ $(($n_back_quote   % 2)) -eq 0 ]] && {
+        BUFFER="${LBUFFER}${gksdud[${KEYS}]}${RBUFFER}"
+        CURSOR+=${#gksdud[${KEYS}]}
+        return 0
+    }
+    BUFFER="${LBUFFER}${KEYS}${RBUFFER}"
+    CURSOR+=${#KEYS}
 }
 
 _complete_dudgks() {
     res=$(python3 $ZSH_HANGUL_DIR/dudgks.py $words[$CURRENT])
     compadd -S "" -U -Q $res
-    compcall -D
 }
 
 zle -N _convert_gksdud
