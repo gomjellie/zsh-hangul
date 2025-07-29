e0569f992c45b541abfce876794babf6c5e3330a
Inkyu Oh <gomjellie@gmail.com>
2025-07-30 06:02:11 +0900

    복사 붙여넣기시 한글 허용

--- zsh-hangul.zsh
+++ zsh-hangul.zsh
@@ -3,8 +3,19 @@ autoload -Uz _convert_gksdud
 
 ZSH_HANGUL_DIR=$(dirname $0)
 
+# 붙여넣기 상태를 추적하는 변수
+_zsh_hangul_is_pasting=0
+
 _convert_gksdud() {
     # widget to convert korean -> english
+    
+    # 붙여넣기 중일 때는 한영변환을 하지 않음
+    if [[ $_zsh_hangul_is_pasting -eq 1 ]]; then
+        BUFFER="${LBUFFER}${KEYS}${RBUFFER}"
+        CURSOR+=${#KEYS}
+        return 0
+    fi
+    
     let n_double_quote=$(echo $LBUFFER |grep -o "\"" |wc -l |xargs)
     let n_single_quote=$(echo $LBUFFER |grep -o "'" |wc -l |xargs)
     let n_back_quote=$(echo $LBUFFER |grep -o "\`" |wc -l |xargs)
@@ -20,12 +31,25 @@ _convert_gksdud() {
     CURSOR+=${#KEYS}
 }
 
+# 사용자 정의 bracketed-paste 함수
+_zsh_hangul_bracketed_paste() {
+    _zsh_hangul_is_pasting=1
+    
+    # 원본 bracketed-paste 동작 수행
+    zle .bracketed-paste
+    
+    _zsh_hangul_is_pasting=0
+}
+
 _complete_dudgks() {
     res=$(python3 $ZSH_HANGUL_DIR/dudgks.py $words[$CURRENT])
     compadd -S "" -U -Q $res
 }
 
 zle -N _convert_gksdud
+
+# bracketed-paste 위젯을 커스터마이징
+zle -N bracketed-paste _zsh_hangul_bracketed_paste
 
 # zle -C _complete_dudgks menu-complete _complete_dudgks
 
